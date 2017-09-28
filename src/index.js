effff23b9a2e2e0e92b255aa0095c25d79f7c57c
lindenii <35da9s1ha>
2017-09-29 01:21:33 +0300

    big digits

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function multiply(first, second) {
-  // your solution
+    var digit = [];
+    var sum = [];
+    var save = 0;
+    /*var row = [];*/
+    first = first.split('').reverse();
+    second = second.split('').reverse();
+    var digit= new Array(first.length + second.length).fill(0);
+
+    for( i = 0; i < second.length; i++){
+        var mas = [];
+        for( j = 0; j < first.length; j++)
+            mas.push(first[j] * second[i]);
+        sum[i] = mas;
+    }
+
+    for ( i = 0; i < second.length; i++)
+        for ( j = 0; j < first.length; j++)
+            digit[i+j] += sum[i][j];
+
+    for (i = 0; i < digit.length; i++) {
+        digit[i] += save;
+        save = Math.floor(digit[i] / 10);
+        digit[i] = digit[i] % 10;
+    }
+
+    if(digit[digit.length-1]==0)
+        digit.pop();
+
+    return digit.reverse().join('');
 }
